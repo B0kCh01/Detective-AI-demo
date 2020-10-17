4f8796d6e5300fc75160ee1ccc2fa630ae12ba52
b0kch01 <nath49ers@gmail.com>
2020-10-17 13:07:09 -0700

    Slight improvemnts

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ AOS.init({
     duration: 1000,
     easing: "easeInOutQuint",
 });
+
+$("h2").hide();
+$("iframe").hover(
+    function() {
+        $("h2").slideDown();
+    },
+    function() {
+        $("h2").slideUp();
+    }
+);
