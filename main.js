0b583cf210cb7cbbac81c43218db55acac16e91a
b0kch01 <nath49ers@gmail.com>
2020-10-17 13:13:36 -0700

    Stop Animation

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ AOS.init({
 $("h2").hide();
 $("iframe").hover(
     function() {
-        $("h2").slideDown();
+        $("h2").stop().slideDown();
     },
     function() {
-        $("h2").slideUp();
+        $("h2").stop().slideUp();
     }
 );
